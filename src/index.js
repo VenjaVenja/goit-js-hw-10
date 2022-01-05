aaf957e076a7ad3a59735e81eed31454a85fa4d8
Denis Venher <87716979+VenjaVenja@users.noreply.github.com>
2022-01-05 21:51:52 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
+import { showCountryList, showCountryCard } from './js/templates';
+// import countryCardTpl from '../src/templates/country-card.hbs';
+// import countrylistTpl from '../src/templates/country-cards.hbs';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBoxEL = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+searchBoxEL.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event) {
+    event.preventDefault();
+    let searchCountry = searchBoxEL.value;
+    console.log(`searchCountry: `, searchCountry);
+    if (searchCountry.trim() === "") {
+        countryListEl.innerHTML = "";
+        countryInfoEl.innerHTML = "";
+        return
+    }
+    fetchCountries(searchCountry.trim())
+        .then(countries => {
+            // console.log(country)
+            if (countries.length > 10){
+                Notify.info('Too many matches found. Please enter a more specific name.');
+                countryListEl.innerHTML = "";
+                countryInfoEl.innerHTML = "";
+                return;
+            }
+            if (countries.length >= 2 && countries.length <= 10) {
+                const listMarkup = countries.map(country => showCountryList(country))
+                countryListEl.innerHTML = listMarkup.join('');
+                countryInfoEl.innerHTML = "";
+
+            }
+            if (countries.length === 1) {
+                const countryCardMarkup = countries.map(country => showCountryCard(country))
+                countryListEl.innerHTML = "";
+                countryInfoEl.innerHTML = countryCardMarkup.join('');
+            }
+        })
+        .catch(error => {
+            Notify.failure('Oops, there is no country with that name');
+            countryListEl.innerHTML = "";
+            countryInfoEl.innerHTML = "";
+            return error
+    })
+};
---
+++ src/js/templates.js
@@ -0,0 +1,29 @@
+export function showCountryList ({ flags, name }){
+    return `
+    <li class = country-item>
+    <img class = 'country-list__flags' src="${flags.svg}" alt="${name.official}" width=50/>
+    <h2 class = country-list__name>${name.official}</h2>
+    </li>
+    `
+}
+
+export function showCountryCard ({ flags, name, capital, population, languages}){
+    return `
+    <div class='wrapper'>
+        <img class='country-flag' src='${flags.svg}' alt='${name.official}' width=100 />
+        <h2 class='country-title'>${name.official}</h2>
+     </div>
+    <div class='wrapper'>
+        <p class='country-text'>Capital:</p>
+        <span class='country-subtext'>${capital}</span>
+    </div>
+    <div class='wrapper'>
+        <p class='country-text'>Population:</p>
+        <span class='country-subtext'>${population}</span>
+    </div>
+    <div class='wrapper'>
+        <p class='country-text'>Languages:</p>
+        <span class='country-subtext'>${Object.values(languages)}</span>
+    </div>
+    `
+}
